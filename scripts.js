b67f2601b2976bbe5dc62581013489dfce2017d4
kkgomez2 <kkristoffergomez@gmail.com>
2020-08-27 21:38:45 -0500

    Shamefully updating templates to use js I should have been using years ago

--- scripts.js
+++ scripts.js
@@ -1,32 +1,31 @@
-$(function() {
-    $(".header__menu-link").click(function() {
-        var sidebar = $(".sidebar").first()
-        if (sidebar.hasClass("sidebar--open")) {
-            sidebar.removeClass("sidebar--open");
-        } else {
-            sidebar.addClass("sidebar--open");
-        }
-    });
-});
+function toggleMenu() {
+  var sidebarClasses = document.getElementById("sidebar").classList;
+
+  if (sidebarClasses.contains("sidebar--open")) {
+    sidebarClasses.remove("sidebar--open");
+  } else {
+    sidebarClasses.add("sidebar--open");
+  }
+}
 
 function addClapToText() {
-    var x = document.getElementById("myText").value;
+  var x = document.getElementById("myText").value;
 
-    var betterText = "";
-    var capitalFlag = true;
-    for (var i = 0; i < x.length; i++) {
-        if (x.charAt(i) == ' ') {
-            betterText += "&#x1F44F;";
-            capitalFlag = true;
-        } else {
-            if (capitalFlag) {
-                betterText += x.substring(i, i + 1).toUpperCase();
-                capitalFlag = false;
-            } else {
-                betterText += x.charAt(i);
-            }
-        }
+  var betterText = "";
+  var capitalFlag = true;
+  for (var i = 0; i < x.length; i++) {
+    if (x.charAt(i) == " ") {
+      betterText += "&#x1F44F;";
+      capitalFlag = true;
+    } else {
+      if (capitalFlag) {
+        betterText += x.substring(i, i + 1).toUpperCase();
+        capitalFlag = false;
+      } else {
+        betterText += x.charAt(i);
+      }
     }
+  }
 
-    document.getElementById("clap").innerHTML = betterText;
+  document.getElementById("clap").innerHTML = betterText;
 }
